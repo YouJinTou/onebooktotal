c46c59fdcb29ee7ad24fd393d5f48a1085314864
YouJinTou <dandreevd@gmail.com>
2019-07-16 21:20:32 +0300

    Added IScraper.

---
+++ OBT/OBT.Core/Abstractions/IScraper.cs
@@ -0,0 +1,11 @@
+﻿using OBT.Core.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace OBT.Core.Abstractions
+{
+    public interface IScraper
+    {
+        Task<IEnumerable<Book>> ScrapeAsync();
+    }
+}
