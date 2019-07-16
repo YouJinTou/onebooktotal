5821e85f75527f01be36b2012665c64fd751ede1
YouJinTou <dandreevd@gmail.com>
2019-07-16 21:21:06 +0300

    Added a lab project.

---
+++ OBT/OBT.Runner/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using OBT.Core.Abstractions;
+using OBT.Core.Scrapers;
+using OBT.Core.Services;
+
+namespace OBT.Runner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddHttpClient()
+                .AddTransient<IHttpService, HttpService>()
+                .BuildServiceProvider();
+            var httpService = serviceProvider.GetService<IHttpService>();
+            var bookogs = new BookogsScraper(httpService);
+
+            bookogs.ScrapeAsync().Wait();
+        }
+    }
+}
