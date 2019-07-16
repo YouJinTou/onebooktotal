2cbfadb5020f49c30ccf4b353caa5f4f72cb9820
YouJinTou <dandreevd@gmail.com>
2019-07-16 21:20:41 +0300

    Added BookogsScraper.

---
+++ OBT/OBT.Core/Scrapers/BookogsScraper.cs
@@ -0,0 +1,26 @@
+﻿using OBT.Core.Abstractions;
+using OBT.Core.Models;
+using OBT.Core.Models.Bookogs;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace OBT.Core.Scrapers
+{
+    public class BookogsScraper : IScraper
+    {
+        private readonly IHttpService httpService;
+
+        public BookogsScraper(IHttpService httpService)
+        {
+            this.httpService = httpService;
+        }
+
+        public async Task<IEnumerable<Book>> ScrapeAsync()
+        {
+            var url = "https://www.bookogs.com/api/browse/book?page=1";
+            var x = await this.httpService.GetAsync<Rootobject>(url);
+
+            return null;
+        }
+    }
+}
