c908782f778cef89de6a93c3bd7f5332cc45ecaf
YouJinTou <dandreevd@gmail.com>
2019-07-16 21:20:09 +0300

    Added Models/Book.

---
+++ OBT/OBT.Core/Models/Book.cs
@@ -0,0 +1,23 @@
+﻿namespace OBT.Core.Models
+{
+    public class Book
+    {
+        public string Isbn10 { get; set; }
+
+        public string Isbn13 { get; set; }
+
+        public string Title { get; set; }
+
+        public string Genre { get; set; }
+
+        public string Author { get; set; }
+
+        public string Language { get; set; }
+
+        public int Year { get; set; }
+
+        public string Format { get; set; }
+
+        public int Pages { get; set; }
+    }
+}
