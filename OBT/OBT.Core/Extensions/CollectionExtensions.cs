37cb7a9fc270af70c430595b567e25e174aebf2f
Dimitar Dimitrov <dandreevd@gmail.com>
2019-07-17 14:05:45 +0300

    Added CollectionExtensions/ToBatches().

---
+++ OBT/OBT.Core/Extensions/CollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace OBT.Core.Extensions
+{
+    public static class CollectionExtensions
+    {
+        public static IEnumerable<IEnumerable<T>> ToBatches<T>(this IEnumerable<T> collection, int batchSize)
+        {
+            var batches = new List<IEnumerable<T>>();
+            
+            for (int b = 0; b < collection.Count(); b += batchSize)
+            {
+                batches.Add(collection.Skip(b).Take(batchSize));
+            }
+
+            return batches;
+        }
+    }
+}
