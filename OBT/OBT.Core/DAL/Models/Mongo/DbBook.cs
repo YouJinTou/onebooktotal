0a1c2d46db6209b12b7d18ee143c87aabec0447b
YouJinTou <dandreevd@gmail.com>
2019-07-25 20:52:53 +0300

    Added Mongo models.

---
+++ OBT/OBT.Core/DAL/Models/Mongo/DbBook.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System.Collections.Generic;
+
+namespace OBT.Core.DAL.Models.Mongo
+{
+    public class DbBook
+    {
+        [BsonId(IdGenerator = typeof(IdGenerator))]
+        public string Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Isbn10 { get; set; }
+
+        public string Isbn13 { get; set; }
+
+        public string Genre { get; set; }
+
+        public IEnumerable<string> Authors { get; set; }
+
+        public string Language { get; set; }
+
+        public int? Year { get; set; }
+
+        public string Format { get; set; }
+
+        public int? Pages { get; set; }
+    }
+}
---
+++ OBT/OBT.Core/DAL/Models/Mongo/IdGenerator.cs
@@ -0,0 +1,35 @@
+﻿using MongoDB.Bson.Serialization;
+using OBT.Core.Tools;
+
+namespace OBT.Core.DAL.Models.Mongo
+{
+    public class IdGenerator : IIdGenerator
+    {
+        public object GenerateId(object container, object document)
+        {
+            var book = document as DbBook;
+
+            Validator.ThrowIfNull(book);
+
+            var authors = string.Join(string.Empty, book.Authors ?? new string[] { });
+
+            unchecked
+            {
+                var hash = 17;
+                hash = hash * 23 + book.Isbn10?.GetHashCode() ?? 3;
+                hash = hash * 23 + book.Isbn13?.GetHashCode() ?? 5;
+                hash = hash * 23 + book.Title?.GetHashCode() ?? 7;
+                hash = hash * 23 + book.Year?.GetHashCode() ?? 11;
+                hash = hash * 23 + book.Format?.GetHashCode() ?? 13;
+                hash = hash * 23 + authors.GetHashCode();
+
+                return hash.ToString();
+            }
+        }
+
+        public bool IsEmpty(object id)
+        {
+            return id == null || string.IsNullOrWhiteSpace(id.ToString());
+        }
+    }
+}
