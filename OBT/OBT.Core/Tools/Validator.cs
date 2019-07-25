625bf15331f2101ea4a4e84458c6dca551ab8ef4
YouJinTou <dandreevd@gmail.com>
2019-07-25 20:53:00 +0300

    Added Validator.

---
+++ OBT/OBT.Core/Tools/Validator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OBT.Core.Tools
+{
+    public static class Validator
+    {
+        public static void ThrowIfNull(object obj)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException();
+            }
+        }
+    }
+}
