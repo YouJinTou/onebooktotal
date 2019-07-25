90056ff4ccdf48844c050128602292313778619e
YouJinTou <dandreevd@gmail.com>
2019-07-25 20:52:46 +0300

    Added Repository.

---
+++ OBT/OBT.Core/Abstractions/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace OBT.Core.Abstractions
+{
+    public interface IRepository<T>
+    {
+        Task AddAsync(T item);
+
+        Task AddManyAsync(IEnumerable<T> items);
+    }
+}
---
+++ OBT/OBT.Core/DAL/Repositories/Repository.cs
@@ -0,0 +1,35 @@
+﻿using MongoDB.Driver;
+using OBT.Core.Abstractions;
+using OBT.Core.Config;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace OBT.Core.DAL.Repositories
+{
+    public class Repository<T> : IRepository<T>
+    {
+        private readonly IMongoCollection<T> collection;
+
+        public Repository(DatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+            this.collection = database.GetCollection<T>(settings.BooksCollectionName);
+        }
+
+        public async Task AddAsync(T item)
+        {
+            await this.collection.InsertOneAsync(item);
+        }
+
+        public async Task AddManyAsync(IEnumerable<T> items)
+        {
+            var options = new InsertManyOptions
+            {
+                IsOrdered = false
+            };
+
+            await this.collection.InsertManyAsync(items, options);
+        }
+    }
+}
