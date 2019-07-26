a9dd4c052acbfc60d36588dbc20dd923be3733b8
Dimitar Dimitrov <dandreevd@gmail.com>
2019-07-26 13:23:27 +0300

    Added GetDeterministicHashCode().

--- OBT/OBT.Core/Extensions/StringExtensions.cs
+++ OBT/OBT.Core/Extensions/StringExtensions.cs
@@ -71,5 +71,28 @@ namespace OBT.Core.Extensions
         {
             return string.IsNullOrWhiteSpace(s) ? (int?)null : DateTime.Parse(s).Year;
         }
+
+        public static int GetDeterministicHashCode(this string s)
+        {
+            unchecked
+            {
+                int hash1 = (5381 << 16) + 5381;
+                int hash2 = hash1;
+
+                for (int i = 0; i < s.Length; i += 2)
+                {
+                    hash1 = ((hash1 << 5) + hash1) ^ s[i];
+
+                    if (i == s.Length - 1)
+                    {
+                        break;
+                    }
+
+                    hash2 = ((hash2 << 5) + hash2) ^ s[i + 1];
+                }
+
+                return hash1 + (hash2 * 1566083941);
+            }
+        }
     }
 }
